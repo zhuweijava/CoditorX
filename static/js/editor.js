d948e3132c0a017fa6d71124511372833eaefc4c
Liang Ding <dl88250@gmail.com>
2015-01-24 18:22:55 +0800

    using codemirror instead of quill

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,66 +1,21 @@
 var editor = {
     channel: undefined,
-    quill: undefined,
+    codemirror: undefined,
     init: function () {
         this._initWS();
-        this._initQuill();
+        this._initCM();
     },
-    _initQuill: function () {
-        editor.quill = new Quill('#quillEditor', {
-            modules: {
-                'toolbar': {container: '#quillToolbar'},
-                'link-tooltip': true,
-                'image-tooltip': true,
-                'multi-cursor': true
-            },
-            theme: 'snow'
-        });
-
-        editor.quill.addModule('authorship', {
-            authorId: coditor.sessionUsername,
-            color: coditor.color,
-            enabled: true
-        });
-
+    _initCM: function () {
         // FIXME: hard coding
         //var docName = coditor.workspace + "/README.txt";
         var docName = "workspaces/admin/workspace/README.txt";
         var doc = OpenDoc(docName);
-        editor.quill.setText(doc.content);
-        
-        var request = newRequest();
-        request.docName = docName;
-        request.offset = 0;
-        request.color = coditor.color;
 
-        $.ajax({
-            async: false,
-            url: "/doc/setCursor",
-            type: "POST",
-            data: JSON.stringify(request),
-            success: function (data) {
-                if (!data.succ) {
-                    return false;
-                }
-            }
-        });
+        var textArea = document.getElementById("editor");
+        textArea.value = doc.content;
 
-        editor.quill.on('text-change', function (delta, source) {
-            if (source === 'api') {
-                console.log("An API call triggered this change.");
-            } else if (source === 'user') {
-//                console.log("A user action triggered this change.");
-//                console.log(delta);
-
-                var request = newRequest();
-                request.cmd = "text-change";
-                request.delta = delta;
-                request.docName = docName;
-                request.user = coditor.sessionUsername;
-                request.color = coditor.color;
-
-                editor.channel.send(JSON.stringify(request));
-            }
+        editor.codemirror = CodeMirror.fromTextArea(textArea, {
+            autofocus: true
         });
     },
     _initWS: function () {
@@ -72,17 +27,6 @@ var editor = {
 
         this.channel.onmessage = function (e) {
             console.log('[editor onmessage]' + e.data);
-
-            var data = JSON.parse(e.data);
-            if (data.cmd === "text-change") {
-                editor.quill.updateContents(data.output);
-                
-                var authorship = editor.quill.getModule('authorship')
-                authorship.addAuthor(data.user, data.color);
-                
-                var cursorManager = editor.quill.getModule('multi-cursor');
-                cursorManager.setCursor(data.user, data.offset, data.user, data.color);
-            }
         };
 
         this.channel.onclose = function (e) {
